1f56ffcbaf3404eb21e757f866a9980897387965
Ben Drucker <bvdrucker@gmail.com>
2018-09-12 21:57:16 -0700

    wip: create the NodeModule main generator

---
+++ node-module/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Generator = require('yeoman-generator')
+const sort = require('sort-package-json')
+
+module.exports = class NodeModule extends Generator {
+  configuring () {
+    const {name, description, github, me} = this.options
+
+    const existing = this.fs.read(this.destionationPath('package.json'))
+    const result = merge(existing, {
+      name,
+      main: 'index.js',
+      version: '0.0.0',
+      description: this.options.description,
+      license: 'MIT',
+      repository: `${github.username}/${name}`,
+      author: me,
+      files: ['*.js']
+    })
+
+    this.fs.write('package.json', JSON.stringify(sort(result)))
+  }
+}
