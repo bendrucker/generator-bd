f0ee39f80265a93becccf69b66ae69ceefecee74
Ben Drucker <bvdrucker@gmail.com>
2018-09-24 21:56:20 -0700

    write index.js file with support for async

--- node-module/index.js
+++ node-module/index.js
@@ -3,16 +3,44 @@
 const Generator = require('yeoman-generator')
 const merge = require('deep-extend')
 const sort = require('sort-package-json')
+const camel = require('camel-case')
+const dedent = require('endent')
 
 module.exports = class NodeModule extends Generator {
   configuring () {
     this._package()
   }
 
+  writing () {
+    this._index()
+  }
+
   installing () {
     this.npmInstall(['standard', 'tape'], {
       'save-dev': true
     })
+  }
+
+  _index () {
+    const main = camel(this.options.name)
+
+    const declaration = [
+      this.options.async && 'async',
+      'function',
+      main,
+      '()'
+    ]
+    .filter(Boolean)
+    .join(' ')
+
+    this.fs.write('index.js', dedent`
+      'use strict'
+
+      module.exports = ${main}
+
+      ${declaration} {
+
+      }`)
   }
   
   _package () {
--- node-module/test.js
+++ node-module/test.js
@@ -3,20 +3,23 @@
 const test = require('blue-tape')
 const yeoman = require('yeoman-test')
 const {existsSync, promises: {readFile}} = require('fs')
-const editorconfig = require('editorconfig')
-const execa = require('execa')
+const vm = require('vm')
+const Module = require('module')
+const {types: {isAsyncFunction}} = require('util')
+
+const options = {
+  name: 'my-pkg',
+  description: 'A great package',
+  github: {username: 'bendrucker'},
+  me: {
+    name: 'Ben Drucker',
+    email: 'bvdrucker@gmail.com',
+    website: 'http://bendrucker.me'
+  }
+}
 
 test('package', async function (t) {
-  await yeoman.run(__dirname).withOptions({
-    name: 'my-pkg',
-    description: 'A great package',
-    github: {username: 'bendrucker'},
-    me: {
-      name: 'Ben Drucker',
-      email: 'bvdrucker@gmail.com',
-      website: 'http://bendrucker.me'
-    }
-  })
+  await yeoman.run(__dirname).withOptions(options)
 
   t.ok(existsSync('./package.json'), 'exists')
   t.deepEqual(JSON.parse(await readFile('./package.json')), {
@@ -34,3 +37,51 @@ test('package', async function (t) {
     files: ['*.js']
   }, 'package written')
 })
+
+test('index', async function (t) {
+  t.test('sync', async function (t) {
+    await yeoman.run(__dirname).withOptions(options)
+
+    t.ok(existsSync('./index.js'), 'exists')
+    const code = await readFile('./index.js', 'utf8')
+    const lines = code.split('\n\n')
+
+    t.deepEqual(lines, [
+      `'use strict'`,
+      'module.exports = myPkg',
+      'function myPkg () {',
+      '}'
+    ], 'renders code')
+    
+    const fn = run(code)
+    t.equal(typeof fn, 'function', 'is function')
+    t.equal(fn.name, 'myPkg', 'named myPkg')
+    t.notOk(isAsyncFunction(fn), 'is sync')
+  })
+
+  t.test('async', async function (t) {
+    await yeoman.run(__dirname).withOptions(Object.assign({async: true}, options))
+
+    t.ok(existsSync('./index.js'), 'exists')
+    const code = await readFile('./index.js', 'utf8')
+    const lines = code.split('\n\n')
+
+    t.deepEqual(lines, [
+      `'use strict'`,
+      'module.exports = myPkg',
+      'async function myPkg () {',
+      '}'
+    ], 'renders code')
+    
+    const fn = run(code)
+    t.equal(typeof fn, 'function', 'is function')
+    t.equal(fn.name, 'myPkg', 'named myPkg')
+    t.ok(isAsyncFunction(fn), 'is async')
+  })
+})
+
+function run (code) {
+  const context = vm.createContext({module: new Module()})
+  vm.runInContext(code, context)
+  return context.module.exports
+}
