8b0a8dcc93134120aeaa9c39214e42180a697536
Ben Drucker <bvdrucker@gmail.com>
2019-01-01 09:21:10 -0800

    spacing

--- generators/docs/index.js
+++ generators/docs/index.js
@@ -11,6 +11,7 @@ module.exports = class Docs extends Generator {
     this.option('email', { type: String, required: true })
     this.option('website', { type: String, required: true })
   }
+  
   async configuring () {
     const { options: me } = this
     this.fs.write('./license', mit(
